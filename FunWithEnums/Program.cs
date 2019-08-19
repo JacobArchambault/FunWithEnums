dbc655cfa22050dfcd6de7ee46a323db631250e1
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-19 11:25:27 -0400

    Add AskForBonus, play with GetUnderLyingType

--- FunWithEnums/Program.cs
+++ FunWithEnums/Program.cs
@@ -10,6 +10,41 @@ namespace FunWithEnums
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Enums *****");
+            // Make an EmpType Variable.
+            EmpType emp = EmpType.Contractor;
+            AskForBonus(emp);
+
+            // Print storage for the enum.
+            Console.WriteLine("EmpType uses a {0} for storage",
+                Enum.GetUnderlyingType(emp.GetType()));
+            Console.ReadLine();
+        }
+        // Enum as parameters
+        static void AskForBonus(EmpType e)
+        {
+            switch (e)
+            {
+                case EmpType.Manager:
+                    Console.WriteLine("How about stock options instead?");
+                    break;
+                case EmpType.Grunt:
+                    Console.WriteLine("You have got to be kidding...");
+                    break;
+                case EmpType.Contractor:
+                    Console.WriteLine("You already get enough cash...");
+                    break;
+                case EmpType.VicePresident:
+                    Console.WriteLine("VERY GOOD, Sir!");
+                    break;
+            }
+        }
+        enum EmpType
+        {
+            Manager, // = 0
+            Grunt, // = 1
+            Contractor, // = 2
+            VicePresident // = 3
         }
     }
 }
